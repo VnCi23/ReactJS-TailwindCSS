ad5b1b50ff5d637fa766c61c1b91efa6ea19e98f
vincechristian23 <vincegaurino@gmail.com>
2023-09-06 16:15:16 +0800

    make it smoother

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
 
 
-<div class="grid grid-cols-3 gap-4">
+<div class="grid grid-cols-3 gap-4 my1.5">
   <div class="relative">
     <img class="w-full" src="https://picsum.photos/400/400?random=1" alt="Random Image 1"/>
     <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
@@ -47,80 +47,39 @@ function App() {
 
 
 
-<div class="grid grid-cols-3 gap-4">
+<div class="grid grid-cols-3 gap-4 my-2.5">
   <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=1" alt="Random Image 1"/>
+    <img class="w-full" src="https://picsum.photos/400/400?random=7" alt="Random Image 1"/>
     <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
       <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
     </div>
   </div>
   <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=2" alt="Random Image 2"/>
+    <img class="w-full" src="https://picsum.photos/400/400?random=8" alt="Random Image 2"/>
     <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
       <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
     </div>
   </div>
   <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=3" alt="Random Image 3"/>
+    <img class="w-full" src="https://picsum.photos/400/400?random=9" alt="Random Image 3"/>
     <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
       <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
     </div>
   </div>
   <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=4" alt="Random Image 4"/>
+    <img class="w-full" src="https://picsum.photos/400/400?random=10" alt="Random Image 4"/>
     <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
       <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
     </div>
   </div>
   <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=5" alt="Random Image 5"/>
+    <img class="w-full" src="https://picsum.photos/400/400?random=11" alt="Random Image 5"/>
     <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
       <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
     </div>
   </div>
   <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=6" alt="Random Image 6"/>
-    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
-      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
-    </div>
-  </div>
-</div>
-
-
-
-<div class="grid grid-cols-3 gap-4">
-  <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=1" alt="Random Image 1"/>
-    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
-      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
-    </div>
-  </div>
-  <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=2" alt="Random Image 2"/>
-    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
-      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
-    </div>
-  </div>
-  <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=3" alt="Random Image 3"/>
-    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
-      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
-    </div>
-  </div>
-  <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=4" alt="Random Image 4"/>
-    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
-      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
-    </div>
-  </div>
-  <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=5" alt="Random Image 5"/>
-    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
-      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
-    </div>
-  </div>
-  <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=6" alt="Random Image 6"/>
+    <img class="w-full" src="https://picsum.photos/400/400?random=12" alt="Random Image 6"/>
     <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
       <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
     </div>
@@ -129,45 +88,249 @@ function App() {
 
 
 
-<div class="grid grid-cols-3 gap-4">
+<div class="grid grid-cols-3 gap-4 my-2.5">
   <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=1" alt="Random Image 1"/>
+    <img class="w-full" src="https://picsum.photos/400/400?random=13" alt="Random Image 1"/>
     <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
       <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
     </div>
   </div>
   <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=2" alt="Random Image 2"/>
+    <img class="w-full" src="https://picsum.photos/400/400?random=14" alt="Random Image 2"/>
     <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
       <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
     </div>
   </div>
   <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=3" alt="Random Image 3"/>
+    <img class="w-full" src="https://picsum.photos/400/400?random=15" alt="Random Image 3"/>
     <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
       <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
     </div>
   </div>
   <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=4" alt="Random Image 4"/>
+    <img class="w-full" src="https://picsum.photos/400/400?random=16" alt="Random Image 4"/>
     <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
       <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
     </div>
   </div>
   <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=5" alt="Random Image 5"/>
+    <img class="w-full" src="https://picsum.photos/400/400?random=17" alt="Random Image 5"/>
     <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
       <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
     </div>
   </div>
   <div class="relative">
-    <img class="w-full" src="https://picsum.photos/400/400?random=6" alt="Random Image 6"/>
+    <img class="w-full" src="https://picsum.photos/400/400?random=18" alt="Random Image 6"/>
     <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
       <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
     </div>
   </div>
 </div>
 
+
+
+<div class="grid grid-cols-3 gap-4 my-2.5">
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=19" alt="Random Image 1"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=20" alt="Random Image 2"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=21" alt="Random Image 3"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=22" alt="Random Image 4"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=23" alt="Random Image 5"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=24" alt="Random Image 6"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="grid grid-cols-3 gap-4 my-2.5">
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=25" alt="Random Image 1"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=26" alt="Random Image 2"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=27" alt="Random Image 3"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=28" alt="Random Image 4"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=29" alt="Random Image 5"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=30" alt="Random Image 6"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="grid grid-cols-3 gap-4 my-2.5">
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=31" alt="Random Image 1"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=32" alt="Random Image 2"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=33" alt="Random Image 3"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=34" alt="Random Image 4"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=35" alt="Random Image 5"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=36" alt="Random Image 6"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="grid grid-cols-3 gap-4 my-2.5">
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=37" alt="Random Image 1"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=38" alt="Random Image 2"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=39" alt="Random Image 3"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=40" alt="Random Image 4"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=41" alt="Random Image 5"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=42" alt="Random Image 6"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="grid grid-cols-3 gap-4 my-2.5">
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=43" alt="Random Image 1"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=44" alt="Random Image 2"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=45" alt="Random Image 3"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=46" alt="Random Image 4"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=47" alt="Random Image 5"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+  <div class="relative">
+    <img class="w-full" src="https://picsum.photos/400/400?random=48" alt="Random Image 6"/>
+    <div class="absolute inset-0 bg-black opacity-0 hover:opacity-75 transition duration-300">
+      <a href="#" class="absolute inset-0 flex items-center justify-center text-white text-lg font-bold uppercase">View</a>
+    </div>
+  </div>
+</div>
 
 
 
